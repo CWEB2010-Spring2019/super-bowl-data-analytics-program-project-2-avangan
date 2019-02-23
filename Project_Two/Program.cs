90fb573700b9b7a89cdaa4e9cb68e5751d3aae7d
avangan <38505183+avangan@users.noreply.github.com>
2019-02-23 00:55:01 -0600

    Update Program.cs

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -1,16 +1,48 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Collections;
+using System.Text;
 
-namespace Project_Two
+
+namespace SuperBowl
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            /**Your application should allow the end user to pass end a file path for output 
-            * or guide them through generating the file.
-            **/
-
+            //Program should read the file from where the project folder exist.
            
+            string Path ="C:\\Users\\kozjohn\\Desktop\\superBowlSuper_Bowl_Project.csv ";
+
+            Console.WriteLine("Welcome to the Super Bowl Stats Sheet  ");
+        
+
+      
+            //textPath variable establishes where the text file will be saved
+            string textPath = "C:\\Users\\kozjohn\\Desktop\\.txt";
+
+            ReadWriteFiles(textPath);
+
         }
+
+        //Method that writes to textPath(includes all queries)
+        static void ReadWriteFiles(string adjustedFilePath, string textPath)
+        {
+            //Create list of objects
+            List<SuperBowl> values = File.ReadAllLines(adjustedFilePath)
+                                             .Skip(1)
+                                             .Select(v => SuperBowl.FromCsv(v))
+                                             .ToList();
+                     
+            StreamWriter sw = new StreamWriter(textPath);
+
+            }
+
+            
+
+
     }
 }
+
