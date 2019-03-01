73973cbe09e33db396acb9f9d50dd0e387089f8e
avangan <38505183+avangan@users.noreply.github.com>
2019-03-01 15:43:11 -0600

    Update Program.cs

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -33,7 +33,7 @@ namespace SuperBowl
             //Create list of objects
             List<SuperBowl> values = File.ReadAllLines(adjustedFilePath)
 
-                     
+              // Created new stream Writer       
             StreamWriter sw = new StreamWriter(textPath);
 
             }
